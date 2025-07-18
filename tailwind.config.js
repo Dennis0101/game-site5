874565496751c898016871701ff2fec572b054b8
Dennis0101 <67768185+Dennis0101@users.noreply.github.com>
2025-07-18 19:41:31 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: { primary: "#0f172a", accent: "#3b82f6" }
+    }
+  },
+  plugins: []
+}
